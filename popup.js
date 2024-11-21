50a16c5cd96d89950fd8945a0b0fe898dfb2a6a4
ziggs.nguyen <ziggs.nguyen@lgepartner.com>
2024-11-21 15:04:47 +0700

    update open link in new tab

--- popup.js
+++ popup.js
@@ -312,3 +312,12 @@ document.getElementById('progressBtn').addEventListener('click', async () => {
         console.error(error);
     }
 });
+
+document.getElementsByTagName("BODY")[0].onclick = function (e) {
+    e = e || event;
+    var target = e.target || e.srcElement;
+    if (target.nodeName != "A") return;
+    var href = target.href;
+    chrome.tabs.create({ url: href });
+    return false;
+};
